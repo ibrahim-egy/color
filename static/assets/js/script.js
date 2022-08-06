e355192c3626b0ef33638b51eb4d85b4ffe61e37
ibrahim-egy <ibrahimtarek.h@gmail.com>
2022-08-06 17:44:25 +0200

    updated styles

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -6,3 +6,17 @@ function show (event) {
     image.style.visibility = 'visible'
     document.getElementById('submit-btn').style.visibility = 'visible'
 }
+
+function copy(i) {
+    var copyText = document.querySelector('.color-' + i)
+
+  /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.value);
+
+    /* Alert the copied text */
+    alert("Copied color in rgb: " + copyText.value)
+}
